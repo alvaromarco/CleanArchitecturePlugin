70533cc2eb6cdc0237947314590e8a7833dae4cc
Alvaro Marco Perez <alvaro.marco@digio.es>
2016-09-30 13:16:12 +0200

    Clean code

--- resources/templates/ParentActivityCleanArch.java
+++ resources/templates/ParentActivityCleanArch.java
@@ -48,12 +48,10 @@ public abstract class ${NAME} extends AppCompatActivity {
 
     @Override protected void onStart() {
         super.onStart();
-        //EventBus.getDefault().register(this);
     }
 
     @Override protected void onStop() {
         super.onStop();
-        //EventBus.getDefault().unregister(this);
     }
 
     @Override protected void onDestroy() {
